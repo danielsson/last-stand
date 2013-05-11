06bace93da575ae4f66b61767b623dc6266cce30
Mattias Danielsson <matdanie@kth.se>
2013-05-11 13:44:50 +0200

    The upgradeview now work.
    This is the first rude implementation.

--- app/src/se/kth/inda/indaprojekt/GameActivity.java
+++ app/src/se/kth/inda/indaprojekt/GameActivity.java
@@ -1,28 +1,22 @@
 package se.kth.inda.indaprojekt;
 
 import se.kth.inda.indaprojekt.GameSurfaceView.OnSurfaceCreatedListener;
+import se.kth.inda.indaprojekt.UpgradeView.OnUpgradedListener;
 import se.kth.inda.indaprojekt.engine.Dimension;
 import se.kth.inda.indaprojekt.engine.GameEngine;
-import se.kth.inda.indaprojekt.engine.Level;
-import se.kth.inda.indaprojekt.engine.Spell;
-import se.kth.inda.indaprojekt.engine.Wizard;
 import se.kth.inda.indaprojekt.engine.GameEngine.GameEngineEventListener;
-import se.kth.inda.indaprojekt.engine.spells.FireBall;
-import se.kth.inda.indaprojekt.engine.spells.ShockwaveSpell;
-import se.kth.inda.indaprojekt.engine.spells.TeleportSpell;
+import se.kth.inda.indaprojekt.engine.Level;
+import se.kth.inda.indaprojekt.engine.Wizard;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 /**
  * An activity that runs a gameengine and renders it using a {@link GameSurfaceView}.
  */
-public class GameActivity extends Activity implements OnSurfaceCreatedListener {
+public class GameActivity extends Activity implements OnSurfaceCreatedListener, OnUpgradedListener {
 	
 	/**
 	 * The game engine.
@@ -33,7 +27,7 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 	private GestureDetector gestureHandler;
 	private GameGestureHandler gameGestureHandler;
 	
-	private LinearLayout upgradeView;
+	private UpgradeView upgradeView;
 	
 	private LevelProgressHandler levelHandler;
 	
@@ -50,11 +44,8 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 				@Override
 				public void run() {
 					if(levelHandler.hasNextLevel()){
-						levelHandler.moveToNextLevel();
-						initiateNextLevel();
+						upgradeView.show(levelHandler.getNumPoints());
 					}
-					else
-						displayUpgradeView();
 				}
 			});
 		}
@@ -65,7 +56,7 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 				
 				@Override
 				public void run() {
-					Toast.makeText(GameActivity.this, "Fail", Toast.LENGTH_LONG).show();
+					Toast.makeText(GameActivity.this, R.string.game_over, Toast.LENGTH_LONG).show();
 				}
 			});
 		}
@@ -85,7 +76,9 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 		gameSurfaceView = (GameSurfaceView) findViewById(R.id.fullscreen_content2);
 		gameSurfaceView.setOnSurfaceCreatedListener(this);
 		
-		upgradeView = (LinearLayout) findViewById(R.id.upgradeLayout);
+		upgradeView = (UpgradeView) findViewById(R.id.upgradeLayout);
+		upgradeView.create(this);
+		
 		
 		engine = new GameEngine(40, gameEventListener);
 		
@@ -105,6 +98,7 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 			return engine;
 		
 		levelHandler = new LevelProgressHandler(new Dimension(view.getWidth(),view.getHeight()), 30, 5);
+		upgradeView.setLevelHandler(levelHandler);
 		
 		initiateNextLevel();
 		
@@ -127,16 +121,11 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 		super.onResume();
 	}
 	
-	public void displayUpgradeView() {
-		upgradeView.setVisibility(LinearLayout.VISIBLE);
-
-		TextView txtNumPoints = (TextView) upgradeView.findViewById(R.id.txtNumPoints);
-		txtNumPoints.setText(
-				String.format(getResources().getString(R.string.num_points), 55)
-			);
-	}
-	
-	public void initiateNextLevel(){
+	/**
+	 * Helper method to start the next level. It makes sure
+	 * that the engine and gesturehandler are still with us.
+	 */
+	private void initiateNextLevel(){
 		
 		Wizard w = levelHandler.addWizard();
 		Level l = levelHandler.getLevel();
@@ -145,6 +134,14 @@ public class GameActivity extends Activity implements OnSurfaceCreatedListener {
 		engine.setCurrentLevel(l);
 		
 		onResume();
+	}
+
+	@Override
+	public void onUpgraded() {
+		upgradeView.hide();
+		
+		levelHandler.moveToNextLevel();
+		initiateNextLevel();
 	}
 
 }
